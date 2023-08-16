2ea472baa33b9a9ff20689ff794750ea71c1ff63
Samrat Ahmmed <samratahammed29@gmail.com>
2023-08-17 00:32:04 +0600

    add deposit section

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,16 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
    const depositField = document.getElementById('deposit-field');
-     const depositAmount = depositField.value;
-     console.log(depositAmount);
+     const nDepositAmount = depositField.value;
+     const nDepositAmountString = parseFloat(nDepositAmount);
+    // show deposit amount
+    const depositTotalElement = document.getElementById('deposit-total');
+   const pDepositTotal = depositTotalElement.innerText;
+  const pDepositTotalString = parseFloat(pDepositTotal);
+
+   const currentDeposit = pDepositTotalString + nDepositAmountString ;
+//    console.log(depositTotal);
+   depositTotalElement.innerText = currentDeposit;
+   depositField.value = '';
+
+
 })
