9ed0fc877529be0be1339c627591d972bac8e543
Samrat Ahmmed <samratahammed29@gmail.com>
2023-08-17 11:43:20 +0600

    add all

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,4 @@
+// deposit button function....
 document.getElementById('btn-deposit').addEventListener('click',function(){
    const depositField = document.getElementById('deposit-field');
      const nDepositAmount = depositField.value;
@@ -8,9 +9,40 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
   const pDepositTotalString = parseFloat(pDepositTotal);
 
    const currentDeposit = pDepositTotalString + nDepositAmountString ;
-//    console.log(depositTotal);
+
    depositTotalElement.innerText = currentDeposit;
    depositField.value = '';
 
+   const balanceTotal = document.getElementById('total-balance');
+   const pBalance = balanceTotal.innerText;
+   const pBalanceInt = parseFloat(pBalance);
+   const currentBalance =  pBalanceInt + nDepositAmountString;
+   balanceTotal.innerText = currentBalance;
+
 
 })
+
+// Withdraw function ............
+document.getElementById('withdraw-button').addEventListener('click' , function(){
+
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = withdrawInput.value;
+    const withdrawInputInt = parseFloat(withdrawAmount);
+    
+    const withdrawBalance = document.getElementById('withdraw-balance');
+    const withdrawInnerText = withdrawBalance.innerText;
+    const withdrawInt = parseFloat(withdrawInnerText);
+    const withdrawTotal = withdrawInputInt + withdrawInt;
+
+
+    withdrawBalance.innerText = withdrawTotal;
+
+    const balanceTotal = document.getElementById('total-balance');
+    const pBalance = balanceTotal.innerText;
+    const pBalanceInt = parseFloat(pBalance);
+    const currentBalance =  pBalanceInt - withdrawInputInt;
+    balanceTotal.innerText = currentBalance;
+
+    withdrawInput.value='';
+})
+
